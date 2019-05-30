bd23a2349c1da89e0ec676dccfc22eb33b5f9a3e
issacgreenfield <issacgreenfield@gmail.com>
2019-05-30 11:42:01 -0700

    Updated style-sheet, getting rid of ghibli code, added description element to cards in scripts.js

--- scripts.js
+++ scripts.js
@@ -30,16 +30,23 @@ request.onload = function () {
             const card = document.createElement('div');
             card.setAttribute('class', 'card');
 
+            // Add title
             const h1 = document.createElement('h1');
             h1.textContent = data.result[i].name;
 
-            // const p = document.createElement('p');
-            // result[i].name = result[i].name.substring(0, 300);
-            // p.textContent = `${result[i].name}...`;
+            // Add photo
+            // const img = document.createElement('img');
+            // data.result[i].source = data.result[i].source;
+
+            // Add description
+            const p = document.createElement('p');
+            data.result[i].name = data.result[i].name.substring(0, 300);
+            p.textContent = `${data.result[i].name}...`;
 
             container.appendChild(card);
+            card.appendChild(img);
             card.appendChild(h1);
-
+            card.appendChild(p);
         }
     } else {
         const errorMessage = document.createElement('marquee');
