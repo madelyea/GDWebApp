b10c154d9c174acd2ebb69cfae6c866b00f3e5bf
Issac Greenfield <issacgreenfield@gmail.com>
2019-06-02 10:29:31 -0700

    Fixed names and links to cardpage and dayin for js and html files. Also got the format to mimic the dayin script request from the api. Does not return results, needs to be fixed

--- dayScript.js
+++ dayScript.js
@@ -6,8 +6,16 @@ cardContainer.setAttribute('class', 'cardContainer');
 app.appendChild(cardContainer);
 var request = new XMLHttpRequest();
 
+// get the date and open the search
+var today = new Date();
+// need to convert month number to name, could use moment instead
+var months = ["january", "february", "march", "april", "may", "june",
+    "july", "august", "september", "october", "november", "december"];
 
-request.open('GET', 'https://archive.org/metadata/october13_201810/files', true);
+request.open('GET', 'https://archive.org/metadata/' + months[today.getMonth()] + ("0" + today.getDate()).slice(-2).toString() + '_' + today.getFullYear().toString() + '10/files', true);
+console.log('https://archive.org/metadata/' + months[today.getMonth()] + ("0" + today.getDate()).slice(-2).toString() + '_' + today.getFullYear().toString() + '10/files');
+
+//request.open('GET', 'https://archive.org/metadata/october13_201810/files', true);
 
 console.log(request.response);
 request.onload = function () {
@@ -16,29 +24,29 @@ request.onload = function () {
     var data = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
         console.log(data);
-        for (i = 0; i < data.result.length; i++) {
-            console.log(data.result[i].name);
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
+        // for (i = 0; i < data.result.length; i++) {
+        //     //console.log(data.result[i].name);
+        //     const card = document.createElement('div');
+        //     card.setAttribute('class', 'card');
 
-            // Add title
-            const h1 = document.createElement('h1');
-            h1.textContent = data.result[i].name;
+        //     // Add title
+        //     const h1 = document.createElement('h1');
+        //     h1.textContent = data.result[i].name;
 
-            // Add photo
-            // const img = document.createElement('img');
-            // data.result[i].source = data.result[i].source;
+        //     // Add photo
+        //     // const img = document.createElement('img');
+        //     // data.result[i].source = data.result[i].source;
 
-            // Add description
-            const p = document.createElement('p');
-            data.result[i].name = data.result[i].name.substring(0, 300);
-            p.textContent = `${data.result[i].name}...`;
+        //     // Add description
+        //     const p = document.createElement('p');
+        //     data.result[i].name = data.result[i].name.substring(0, 300);
+        //     p.textContent = `${data.result[i].name}...`;
 
-            cardContainer.appendChild(card);
-            // card.appendChild(img);
-            card.appendChild(h1);
-            card.appendChild(p);
-        }
+        //     cardContainer.appendChild(card);
+        //     // card.appendChild(img);
+        //     card.appendChild(h1);
+        //     card.appendChild(p);
+        // }
     } else {
         const errorMessage = document.createElement('marquee');
         errorMessage.textContent = `I'm sorry, the request to the external api is experienceing difficulties`;
