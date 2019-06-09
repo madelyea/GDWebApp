2850209803d58c855de29a0afaf379627450bd45
Madeleine Elyea <MadeleineElyea@Madeleines-MacBook-Pro.local>
2019-06-08 17:31:22 -0700

    able to access individual show page's metadata in a json object. Edited font to sans-serif. Images in correct placement on card.

--- injectionscript.js
+++ injectionscript.js
@@ -4,7 +4,7 @@
 var GD =
 {
     day: null,
-
+    img: null,
     this_day_in_history: function (obj) {
         this.day = obj; //save away
     },
@@ -37,10 +37,27 @@ var GD =
             var title = hit.title;
             var desc = hit.description;
             var ident = hit.identifier;
+            var img;
+            var dir;
+            //access individual show's metadata to get info for image and embedded music player
+            //sourced from: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON"
+            var requestURL = "https://archive.org/metadata/" + ident;
+            var request = new XMLHttpRequest();
+            request.open('GET', requestURL);
+            request.responseType = 'json';
+            request.send();
+            request.onload = function () {
+                var showData = request.response;
+                console.log(showData['dir']);
+                console.log(showData.files[0].name);
+                dir = showData.dir;
+                imgPathname = showData.files[0].name;
+                console.log(imgPathname);
+            }
 
 
             str += '<a class="card" href=https://archive.org/details/' + ident + '><h1>' + title + '</h1>' +
-                '<p>' + desc + '</p></a>';
+                '<p><img src = "https://ia600201.us.archive.org/0/items/gd1995-06-04.sennME40.sturtevant-swayne.85716.flac1648/gd95-06-04s1t03.png"' + desc + '</p></a>';
         }
 
         obj.innerHTML = str;
